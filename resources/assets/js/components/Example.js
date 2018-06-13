d172c64b97d9bf69e3b56ccf7801bad70c739c1b
mvaughan <magnus.vaughan@gmail.com>
2018-06-13 22:44:09 +0100

    More work on sharing state between parent and child component via props

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Monster from './Monster';
 
 export default class Example extends Component {
     constructor() {
     
         super();
+
+        var monster1Id = this.getRandomMonsterId();
+        var monster2Id = this.getRandomMonsterId();
         //Initialize the state in the constructor
         this.state = {
-            monster: [],
-            monster1Id: null,
-            monster2Id: null
-        }
+            monster1Id: monster1Id,
+            monster2Id: monster2Id,
+            monster1: this.getRandomMonster(monster1Id, 'monster1'),
+            monster2: this.getRandomMonster(monster2Id, 'monster2')
+        };
     }
 
-    getRandomMonsterId = () => {
+    componentDidMount() {
+
+    }
+
+    getRandomMonsterId() {
         return Math.ceil((Math.random() * 31));
     }
 
-    getRandomMonster = function() {
-
-        /* fetch API in action */
-        
-        this.setState({monster1Id: this.getRandomMonsterId()});
-        this.setState({monster2Id: this.getRandomMonsterId()});
-
+    getRandomMonster(monsterId, stateKey) {
+        fetch('/monsters/'+ monsterId )
+        .then(response => {
+            return response.json();
+        })
+        .then(monster => {
+            //Fetched product is stored in the state
+            this.setState({ [stateKey]: monster });
+        });
     }
+
     render() {
         return (
             <div className="container">
@@ -35,10 +47,10 @@ export default class Example extends Component {
                 </div>
                 <div className="row">
                     <div className="col-md-6">
-                        <Monster monsterId={monster1Id} />
+                        <Monster monster={this.state.monster1} monsterId={this.state.monster1Id} />
                     </div>
                     <div className="col-md-6">
-                        <Monster monsterId={monster2Id} />
+                        <Monster monster={this.state.monster2} monsterId={this.state.monster2Id} />
                     </div>
                 </div>
             </div>
--- resources/assets/js/components/Monster.js
+++ resources/assets/js/components/Monster.js
@@ -5,22 +5,23 @@ import Example from './Example';
 export default class Monster extends Component {
 
     render() {
+        console.log("propsy", this.props);
         return (
             <div className="card">
-                <h2 className="card-title">{this.state.monster.name}</h2>
-                <img className="card-img-top" style={{width: 526 + 'px', height: 480 + 'px'}} src={"/img/" + this.state.monsterId + ".png"} alt=""/>
+                <h2 className="card-title">{this.props.monster.name}</h2>
+                <img className="card-img-top" style={{width: 526 + 'px', height: 480 + 'px'}} src={"/img/" + this.props.monsterId + ".png"} alt=""/>
                 <ul>
                     <li>Physical Strength: 
-                    <button type="button" className="btn btn-danger">{this.state.monster.physical_strength}</button>
+                    <button type="button" className="btn btn-danger">{this.props.monster.physical_strength}</button>
                     </li>
                     <li>Fear Factor: 
-                    <button type="button" className="btn btn-danger">{this.state.monster.fear_factor}</button>
+                    <button type="button" className="btn btn-danger">{this.props.monster.fear_factor}</button>
                     </li>
                     <li>Killing Power:
-                    <button type="button" className="btn btn-danger">{this.state.monster.killing_power}</button>
+                    <button type="button" className="btn btn-danger">{this.props.monster.killing_power}</button>
                     </li>
                     <li>Horror Rating:
-                    <button type="button" className="btn btn-danger">{this.state.monster.horror_rating}</button>
+                    <button type="button" className="btn btn-danger">{this.props.monster.horror_rating}</button>
                     </li>
                 </ul>
             </div>
